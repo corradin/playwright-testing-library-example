f497dc36f1f4dae5dd9e8dabc5b00d411ffa0fbd
Riccardo Corradin <riccardocorradin@goodhabitz.com>
2022-01-24 11:27:57 +0100

    Removed console log

--- meetup-login.test.ts
+++ meetup-login.test.ts
@@ -1,7 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('Navigate to notifications', async ({ page }) => {
-    console.log(process.env.MEETUP_USERNAME);
     await page.goto('https://www.meetup.com/notifications');
     await expect(page.locator(`"Nothing here yet"`).first()).toBeVisible();
 });
