0347606e15566225b46c30e91e35267eb55d4877
Riccardo Corradin <riccardo.corradin@live.com>
2022-09-04 20:54:32 +0200

    Updated and fixed tests

--- meetup-login.test.ts
+++ meetup-login.test.ts
@@ -2,10 +2,10 @@ import { expect, test } from '@playwright/test';
 
 test('Navigate to notifications', async ({ page }) => {
     await page.goto('https://www.meetup.com/notifications');
-    await expect(page.locator(`"Nothing here yet"`).first()).toBeVisible();
+    await expect(page.locator(`"Exciting new group for you!"`).first()).toBeVisible();
 });
 
 test('Navigate to payment methods', async ({ page }) => {
     await page.goto('https://www.meetup.com/account/payment-methods');
-    await expect(page.locator(`"You don't have any saved cards."`).first()).toBeVisible();
+    await expect(page.locator(`"You don't have any saved cards for your organizer subscription"`).first()).toBeVisible();
 });
