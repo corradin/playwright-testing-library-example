8f9278a14ca8b70e6081c354c0a1ef417689c920
Riccardo Corradin <riccardo.corradin@live.com>
2023-04-05 13:09:33 +0200

    Fixed test

--- meetup-login.test.ts
+++ meetup-login.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('Navigate to notifications', async ({ page }) => {
     await page.goto('https://www.meetup.com/notifications');
-    await expect(page.locator(`"Exciting new group for you!"`).first()).toBeVisible();
+    await expect(page.getByText("This week")).toBeVisible();
 });
 
 test('Navigate to payment methods', async ({ page }) => {
