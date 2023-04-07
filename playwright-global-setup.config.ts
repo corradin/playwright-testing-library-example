3d2fd8f2844524aa438d4846a6f95250ed3721f2
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-04-07 13:38:57 +0200

    Added global setup to compare with the new improved dependencies method

---
+++ playwright-global-setup.config.ts
@@ -0,0 +1,49 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from 'playwright';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const storageStatePath = 'storage-state/storageState.json';
+
+const config: PlaywrightTestConfig = {
+  globalSetup: './global-setup',
+  use: {
+    trace: 'retain-on-failure',
+    storageState: storageStatePath,
+    headless: false,
+  },
+  projects: [
+    {
+      name: 'Chrome',
+      use: {
+        browserName: 'chromium',
+        storageState: storageStatePath,
+      },
+    },
+    {
+      name: 'Desktop Safari',
+      use: {
+        browserName: 'webkit',
+        viewport: { width: 1200, height: 750 },
+      },
+    },
+    // Test against mobile viewports.
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5'],
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 800, height: 600 },
+      },
+    },
+  ],
+};
+export default config;
