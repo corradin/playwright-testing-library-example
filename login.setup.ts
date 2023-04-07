707203c45a7ed494869afc68f2b87379608675f0
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-04-07 13:06:05 +0200

    Web first assertions and update deprecated method

--- login.setup.ts
+++ login.setup.ts
@@ -6,12 +6,12 @@ const password = process.env.MEETUP_PASSWORD ?? '';
 
 setup('Login a user', async ({ page }) => {
   await page.goto('https://www.meetup.com/login');
-  await page.locator('id=email').fill(username);
-  await page.locator('id=current-password').fill(password);
+  await page.getByLabel('Email').fill(username);
+  await page.getByLabel('Password').first().fill(password);
 
   await Promise.all([
-    page.waitForNavigation(),
-    page.locator('button[type=submit] >> "Log in"').click(),
+    page.waitForURL('https://www.meetup.com/home/**'),
+    page.getByRole('button', { name: 'Log in',  exact: true}).click(),
   ]);
 
   await page.context().storageState({
